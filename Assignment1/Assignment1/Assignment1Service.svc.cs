ffea1b9456e3fe3f6295640249ad94586fa61941
darshanpatoliya <darshanpatoliya05@gmail.com>
2022-10-11 12:23:56 -0400

    Added the basic code

---
+++ Assignment1/Assignment1/Assignment1Service.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Assignment1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IAssignment1Service
+    {
+        public string primeCheck(int number)
+        {
+            int i;
+            int temp = number / 2;
+
+            for (i = 2; i <= temp; i++)
+            {
+                if (number % i == 0)
+                {
+                    break;
+                }
+            }
+            if (i > temp)
+            {
+                return "Number is Prime";
+            }
+            else
+            {
+                return "Number is not Prime";
+            }
+        }
+    }
+}
---
+++ Assignment1/Assignment1Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assignment1Reference.Assignment1ServiceClient client = new Assignment1Reference.Assignment1ServiceClient();
+            string choice = "";
+            while (!choice.Equals("5"))
+            {
+                Console.WriteLine("Assignment1");
+                Console.WriteLine("1. PrimeNumber");
+                Console.WriteLine("2. Sum of Digits");
+                Console.WriteLine("3. Reverse a String");
+                Console.WriteLine("4. Print HTML tags");
+                Console.WriteLine("5. Sort 5 Numbers");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("Enter your choice: ");
+
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Enter Number 1: ");
+                        int n = int.Parse(Console.ReadLine());
+                        
+                        string result = client.primeCheck(n);
+                        Console.WriteLine(result);
+
+                        break;
+                    case "2":
+                        
+
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                }
+            }
+        }
+    }
+}
